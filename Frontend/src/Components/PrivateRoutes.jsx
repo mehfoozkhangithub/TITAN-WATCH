6b376e08d8f67fc7ddabde0e3e0a04cedd3ea56d
mehfoozkhangihub <khanmehfooz78603@gmail.com>
2026-04-01 17:06:53 +0530

    Code

--- Frontend/src/Components/PrivateRoutes.jsx
+++ Frontend/src/Components/PrivateRoutes.jsx
@@ -5,7 +5,7 @@ export const PrivateRoutes = ({ children }) => {
   const { isAuth } = useSelector((store) => store.auth);
   if (!isAuth) {
     return <Navigate to={'/login'} />;
-  }
+  } /* siraj bhai ka chuchi mast h usse dudh niklna hai or sab ko batna hai */
 
   return children;
 };
